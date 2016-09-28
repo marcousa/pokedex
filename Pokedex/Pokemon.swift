721abc296026c1523ab8d1a52767e20182f0bdc7
marcousa <marcodefilippo@gmail.com>
2016-09-28 16:42:55 -0400

    initial collectionView setup

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Marco De Filippo on 9/28/16.
+//  Copyright © 2016 2AM App Labs. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
