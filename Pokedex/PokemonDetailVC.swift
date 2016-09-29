7f85c05d77dd5db05887ffb200ffbcf4d6bded59
marcousa <marcodefilippo@gmail.com>
2016-09-29 16:02:18 -0400

    added a detailsVC

---
+++ Pokedex/PokemonDetailVC.swift
@@ -0,0 +1,22 @@
+//
+//  PokemonDetailVC.swift
+//  Pokedex
+//
+//  Created by Marco De Filippo on 9/29/16.
+//  Copyright © 2016 2AM App Labs. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
